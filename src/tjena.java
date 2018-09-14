61a97066d8a637be4f3f4c71ed6ab6ac6d3c8d78
Borg22 <joahngit config --global user.email joahan.borg2001@outlook.com>
2018-09-14 15:04:51 +0200

    steg 3

--- src/tjena.java
+++ src/tjena.java
@@ -9,26 +9,39 @@ public class tjena {
 		
 		int max = 9999999 ;
 		int min = -9999999 ; 
+		int medel = 
+		int summa = tal1 + tal2 + tal3 + tal4 + tal5 + tal6 + tal7 + tal8 + tal9 + tal10
 		
+		dubble medel1 = summa / 5; // 5
+		dubble medel2 = summa /5,0 ; // 5,6
+		
+		
+		//Läs in tal 1
 		int tal1 = input.nextInt() ;
-		
+		//Läs in tal 2
 		int tal2 = input.nextInt() ;
-		
+		//Läs in tal 3
 		int tal3 = input.nextInt() ;
-		
+		//Läs in tal 4
 		int tal4 = input.nextInt() ;
-		
+		//Läs in tal 5
 		int tal5 = input.nextInt() ;
-		
+		//Läs in tal 6
 		int tal6 = input.nextInt() ;
-		
+		//Läs in tal 7
 		int tal7 = input.nextInt() ;
-		
+		//Läs in tal 8
 		int tal8 = input.nextInt() ;
-		
+		//Läs in tal 9
 		int tal9 = input.nextInt() ;
-		
+		//Läs in tal 10
 		int tal10 = input.nextInt() ;
+		
+		
+		System.out.println("Minsta" + min);
+		System.out.println("Medel" + medel);
+		System.out.println("Största" + max);
+		
 		
 		
 	}
