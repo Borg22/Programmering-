c3b94854d5c6d3c39894befb50f9e400c267f850
Borg22 <joahngit config --global user.email joahan.borg2001@outlook.com>
2018-09-14 14:50:43 +0200

    steg 2

--- src/tjena.java
+++ src/tjena.java
@@ -10,16 +10,26 @@ public class tjena {
 		int max = 9999999 ;
 		int min = -9999999 ; 
 		
-		int tal1 = 
-		int tal2 =
-		int tal3 =
-		int tal4 =
-		int tal5 =
-		int tal6 =
-		int tal7 =
-		int tal8 =
-		int tal9 =
-		int tal10 =
+		int tal1 = input.nextInt() ;
+		
+		int tal2 = input.nextInt() ;
+		
+		int tal3 = input.nextInt() ;
+		
+		int tal4 = input.nextInt() ;
+		
+		int tal5 = input.nextInt() ;
+		
+		int tal6 = input.nextInt() ;
+		
+		int tal7 = input.nextInt() ;
+		
+		int tal8 = input.nextInt() ;
+		
+		int tal9 = input.nextInt() ;
+		
+		int tal10 = input.nextInt() ;
+		
 		
 	}
 }
