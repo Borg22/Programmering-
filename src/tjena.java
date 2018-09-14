c0b40b160393dec0c37d77c3323cbe993fbf404c
Borg22 <joahngit config --global user.email joahan.borg2001@outlook.com>
2018-09-14 14:47:02 +0200

    tja

---
+++ src/tjena.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class tjena {
+
+	
+	public static void main(String[] args) {
+		
+		Scanner input = new Scanner(System.in);
+		
+		int max = 9999999 ;
+		int min = -9999999 ; 
+		
+		int tal1 = 
+		int tal2 =
+		int tal3 =
+		int tal4 =
+		int tal5 =
+		int tal6 =
+		int tal7 =
+		int tal8 =
+		int tal9 =
+		int tal10 =
+		
+	}
+}
