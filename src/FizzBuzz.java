d7f6d35816470ddd7cbbedddf738a4c7e4c9d9e0
Borg22 <joahngit config --global user.email joahan.borg2001@outlook.com>
2018-09-28 14:22:44 +0200

    klar med läxan

--- src/FizzBuzz.java
+++ src/FizzBuzz.java
@@ -12,7 +12,19 @@ public static void main(String[] args) {
 	
 	for(int i = 1; i<=n; i++) {
 		if(i % x == 0 && i % y == 0) {
-	}
+			System.out.println("FizzBuzz");
+		}
+			else if (i % y == 0) {
+				System.out.println("Buzz");
+			}
+			else if (i % x == 0) {
+				System.out.println("Fizz");
+			}
+			else { 
+				System.out.println(i);
+			}
+		}
+	
 			
 			
 	
