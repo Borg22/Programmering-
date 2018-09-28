2324c5f484855783dc21bd9cc3e2da559a7db0cf
Borg22 <joahngit config --global user.email joahan.borg2001@outlook.com>
2018-09-28 14:18:01 +0200

    Uppgift

---
+++ src/FizzBuzz.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class FizzBuzz {
+	
+public static void main(String[] args) {
+	
+	Scanner input = new Scanner(System.in);
+	
+	int x = input.nextInt();
+	int y = input.nextInt();
+	int n = input.nextInt();
+	
+	for(int i = 1; i<=n; i++) {
+		if(i % x == 0 && i % y == 0) {
+	}
+			
+			
+	
+}
+		
+	}	
+
+
