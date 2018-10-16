5a2959726352562dddfd4ec5113692e6ae7f2f0f
Borg22 <joahngit config --global user.email joahan.borg2001@outlook.com>
2018-10-16 14:37:41 +0200

    Steg 1

---
+++ src/steg3.java
@@ -0,0 +1,20 @@
+ 
+public class steg3 {
+	
+
+	public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+        
+        //System.out.println(volume(sc.nextDouble()));
+        
+        //System.out.println(reverse(sc.nextLine()));
+        
+        System.out.println(count(sc.nextLine(), sc.next().charAt(0)));
+        
+        //System.out.println(sjorovare(sc.nextLine()));
+        
+
+
+}
+	}
