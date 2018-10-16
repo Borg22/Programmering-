fe848ab974d1015af66bc8cda0206614207cd8fc
Borg22 <joahngit config --global user.email joahan.borg2001@outlook.com>
2018-10-16 14:47:07 +0200

    steg 1

--- src/steg3.java
+++ src/steg3.java
@@ -6,17 +6,18 @@ public class steg3 {
         
         Scanner sc = new Scanner(System.in);
         
-        //System.out.println(volume(sc.nextDouble()));
+        System.out.println(volume(sc.nextDouble()));
         
         //System.out.println(reverse(sc.nextLine()));
         
-        System.out.println(count(sc.nextLine(), sc.next().charAt(0)));
+       // System.out.println(count(sc.nextLine(), sc.next().charAt(0)));
         
         //System.out.println(sjorovare(sc.nextLine()));
         
 
 
 }
+	
     public static double volume(double radius) {
         
         double volume = radius * radius * radius * 3.14 * 4 / 3;
